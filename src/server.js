a64dc51454939e0fc40c7e0cc8bebaeca2f13c86
Junior Volonte <juniorvolontedev@gmail.com>
2023-06-26 09:22:22 -0300

    query params

--- src/server.js
+++ src/server.js
@@ -8,6 +8,11 @@ app.get('/message/:id/:user', (request, response) => {
   response.send(`ID da mensagem: ${id}. Para o user: ${user}`);
 });
 
+app.get('/users', (request, response) => {
+  const { page, limit } = request.query;
+  response.send(`Page: ${page} - Limit: ${limit}`);
+});
+
 const PORT = 3333;
 
 app.listen(PORT, () =>
