186c7ce35ca10e8e82bab6eeb0d28a38f4bac530
Junior Volonte <juniorvolontedev@gmail.com>
2023-06-26 09:12:18 -0300

    route params

--- src/server.js
+++ src/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.send('Hello, world!');
+app.get('/message/:id/:user', (request, response) => {
+  const { id, user } = request.params;
+
+  response.send(`ID da mensagem: ${id}. Para o user: ${user}`);
 });
 
 const PORT = 3333;
