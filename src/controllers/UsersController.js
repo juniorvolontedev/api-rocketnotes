bdcbf5e92bbcdbc1980e77842427799fff345afe
Junior Volonte <juniorvolontedev@gmail.com>
2023-06-26 21:01:26 -0300

    controllers

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,11 @@
+class UsersController {
+  /**
+   * index - GET para listar vários registros
+   * show - GET para exibir um registro específico
+   * create - POST para criar um registro
+   * update - PUT atualizar um registro
+   * delete - DELETE remover um registro
+   */
+}
+
+module.exports = UsersController;
